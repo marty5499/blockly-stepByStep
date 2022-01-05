e50bd7735ec02373b1cd2a8776bdbe9ad6b48aec
Marty <marty@kingkit101.com>
2022-01-05 18:07:45 +0800

    add menu , mouse class ,fix bug

--- menu.js
+++ menu.js
@@ -1,3 +1,91 @@
+class Cursor {
+  async init(x, y) {
+    Cursor.POINTER = 0;
+    Cursor.MOUSEDOWN = 1;
+    this.ready = false;
+    this.imgClick = 'https://www.freeiconspng.com/uploads/mouse-cursor-click-png-outline-2.png';
+    this.imgCursor = 'https://i.pinimg.com/favicons/19638dc8bbdda1dc864a426073a6efb1d0fbc992e66c90dc527993e3.ico?a0d9215bcdb6521a918f0c76255eea7e';
+    this.eleClick = await this.loadImage(this.imgClick, 32, 32);
+    this.ele = await this.loadImage(this.imgCursor, 32, 32);
+    this.ele.style.position = 'absolute';
+    this.ele.style.display = '';
+    this.ele.style.zIndex = 999;
+    this.set(x, y);
+    document.body.appendChild(this.ele);
+    this.ready = true;
+  }
+
+  show() {
+    this.ele.style.display = '';
+  }
+
+  hide() {
+    this.ele.style.display = 'none';
+  }
+
+  type(n) {
+    switch (n) {
+      case 0:
+        this.ele.src = this.imgCursor;
+        break;
+      case 1:
+        this.ele.src = this.imgClick;
+        break;
+    }
+  }
+
+  set(x, y) {
+    this.x = x;
+    this.y = y;
+    this.ele.style.top = y + 'px';
+    this.ele.style.left = x + 'px';
+  }
+
+  async click() {
+    this.ele.src = this.imgClick;
+    await new Promise(r => setTimeout(r, 1000 /*ms*/));
+    this.ele.src = this.imgCursor;
+  }
+
+  async moveToElement(ele, biasX, biasY, speedSec) {
+    while (!this.ready) {
+      await new Promise(r => setTimeout(r, 100 /*ms*/));
+    }
+    var pos = ele.getBoundingClientRect();
+    await this.move(pos.x + biasX, pos.y + biasY, speedSec);
+  }
+
+  async move(ex, ey, speedSec) {
+    var chunk = speedSec * 100;
+    var dx = (ex - this.x) / chunk;
+    var dy = (ey - this.y) / chunk;
+    console.log("dx:", dx, " dy:", dy, "chunk:", chunk);
+    for (var i = 0; i < speedSec * 100; i++) {
+      this.x += dx;
+      this.y += dy;
+      this.set(this.x, this.y);
+      await new Promise(r => setTimeout(r, 1 /*ms*/));
+    }
+  }
+
+  async loadImage(url, width, height) {
+    return new Promise((resolve, reject) => {
+      var img = new Image(width, height);
+      document.body.appendChild(img);
+      img.src = url;
+      img.width = width;
+      img.height = height;
+      img.style.position = 'absolute';
+      img.style.display = 'none';
+      img.onload = async () => {
+        //console.log("Image Loaded:", url);
+        img.style.display = '';
+        resolve(img);
+      };
+    });
+  };
+}
+
 class Block {
   constructor(ele) {
     this.ele = ele;
@@ -6,11 +94,11 @@ class Block {
   }
   delay(sec) {
     Block.delaySec = sec;
-    return this;  
+    return this;
   }
   speed(sec) {
     Block.speedSec = sec;
-    return this;  
+    return this;
   }
   async mouseOver() {
     var rect = this.ele;
@@ -24,29 +112,32 @@ class Block {
     await new Promise(r => setTimeout(r, Block.delaySec * 1000));
     return rect;
   }
-  async moveTo(ex, ey, connect) {
+  async moveTo(ex, ey, connect, cursor, cx, cy) {
     var self = this;
     var rect = this.ele;
     function move(x, y) {
       return new MouseEvent('mousemove', {
-        clientX: rect.getBoundingClientRect().left + x,
-        clientY: rect.getBoundingClientRect().top + y,
+        clientX: x,
+        clientY: y,
         bubbles: true,
         cancelable: true
       });
     }
     var mx = rect.getBoundingClientRect().left;
-    var my = 0;
+    var my = rect.getBoundingClientRect().top;
     rect.dispatchEvent(self.getEvent('mousedown'));
     var chunk = Block.speedSec * 1000;
-    var dx = Math.abs(mx - ex) / chunk;
-    var dy = ey / chunk;
-    //console.log("dx:",dx," dy:",dy,"chunk:",chunk);
+    var dx = (ex - mx) / chunk;
+    var dy = (ey - my) / chunk;
+    console.log("dx:", dx, " dy:", dy, "chunk:", chunk);
     for (var i = 0; i < Block.speedSec * 1000; i++) {
+      console.log("mx:", mx, " my:", my, "chunk:", chunk);
       mx += dx;
       my += dy;
       rect.dispatchEvent(move(mx, my));
-      await new Promise(r => setTimeout(r, 1 /*ms*/ ));
+      var pos = this.ele.getBoundingClientRect();
+      cursor.set(cx + mx, cy + my);
+      await new Promise(r => setTimeout(r, 1 /*ms*/));
     }
     //console.log("over..ex:", mx, ",ey:", my);
     if (!connect) {
@@ -100,33 +191,66 @@ class Menu {
       this.data[this.list[i].innerHTML] = new Item(this.list[i].parentElement);
     }
   }
-  open(str) {
+  select(str) {
     return this.data[str];
   }
 }
 
-class AIMenu {
-  constructor() {
-    this.list = document.getElementsByClassName('blocklyTreeLabel');
-    this.data = {}
-    for (var i = 0; i < this.list.length; i++) {
-      this.data[this.list[i].innerHTML] = new Item(this.list[i].parentElement.parentElement);
-    }
+class Mouse {
+  constructor(x, y) {
+    this.menu = new Menu();
+    this.cursor = new Cursor();
+    this.runButton = document.getElementById('runButton');
+    this.biasX = 50;
+    this.biasY = 8;
+    var self = this;
+    setTimeout(async function () {
+      await self.cursor.init(x, y);
+    }, 0);
   }
-  open(str) {
-    return this.data[str];
+
+  async wait(sec) {
+    await new Promise(r => setTimeout(r, sec * 1000));
+    return this;
+  }
+
+  async clickItem(itemName) {
+    this.item = this.menu.select(itemName);
+    await this.cursor.moveToElement(this.item.ele, this.biasX, this.biasY, 2);
+    this.cursor.type(Cursor.MOUSEDOWN);
+    await new Promise(r => setTimeout(r, 1500));
+    this.item.click();
+    this.cursor.type(Cursor.POINTER);
+    return this;
+  }
+
+  async moveBlockTo(idxBlock, moveToX, moveToY, attach) {
+    var block = this.item.select(idxBlock);
+    await new Promise(r => setTimeout(r, 500));
+    await this.cursor.moveToElement(block.ele, this.biasX, this.biasY, 2);
+    this.cursor.type(Cursor.MOUSEDOWN);
+    await new Promise(r => setTimeout(r, 500));
+    await block.mouseOver();
+    await block.moveTo(moveToX, moveToY, attach, this.cursor, this.biasX, this.biasY);
+    this.cursor.type(Cursor.POINTER);
+    await new Promise(r => setTimeout(r, 500));
+  }
+
+  async clickRunButton() {
+    await this.cursor.moveToElement(this.runButton, 25, 25, 5);
+    this.runButton.click();
   }
 }
 
-var menu = new Menu();
-var block = menu.open('開發板').click().select(0);
-await block.mouseOver();
-await block.moveTo(300, 50,attach = false);
+var mouse = new Mouse(300, 300);
 
-var block = menu.open('矩陣 LED').click().select(1);
-await block.mouseOver();
-await block.moveTo(360, 40,attach = true);
+await mouse.clickItem('開發板');
+await mouse.moveBlockTo(0, 300, 150, attach = false);
 
-var block = menu.open('矩陣 LED').click().select(2);
-await block.mouseOver();
-await block.moveTo(360, 0,attach = true);
+await mouse.clickItem('矩陣 LED');
+await mouse.moveBlockTo(8, 340, 180, attach = true);
+
+await mouse.clickItem('怪獸控制');
+await mouse.moveBlockTo(0, 340, 210, attach = true);
+
+await mouse.clickRunButton();
